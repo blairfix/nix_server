61ecde8103a843ed0c3ee9d39359a784457790d4
blairfix <blairfix@fastmail.com>
2025-02-04 11:32:09 -0700

    removed nextcloud

--- server_packages.nix
+++ server_packages.nix
@@ -8,16 +8,4 @@
     };
 
 
-    # nextcloud
-    #-------------------------------------------
-
-    environment.etc."nextcloud-admin-pass".text = "test123";
-    services.nextcloud = {
-	enable = true;
-	package = pkgs.nextcloud29;
-	hostName = "localhost";
-	config.adminpassFile = "/etc/nextcloud-admin-pass";
-    };
-
-
 }
