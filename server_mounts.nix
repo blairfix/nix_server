d8dfa5ca63a8b2b8c9e888c26d2de0854206eaa2
blairfix <blairfix@fastmail.com>
2025-02-04 06:41:43 -0700

    added drive mounts

--- server_mounts.nix
+++ server_mounts.nix
@@ -1,13 +1,24 @@
 { config, ... }:
 {
 
-    fileSystems."/media/blair/multimedia" =
-    { device = "/dev/disk/by-uuid/UUID=a2352a98-821a-4ae4-8a10-a3228d356da5";
+    # home
+    fileSystems."/home" =
+    { 
+	device = "/dev/disk/by-uuid/UUID=c18c0719-c196-4acd-a155-947e6262b933";
 	fsType = "ext4";
     };
 
+    # multimedia
+    fileSystems."/media/blair/multimedia" =
+    { 
+	device = "/dev/disk/by-uuid/UUID=f8d56395-fc2e-4add-8bc0-af96159f068e";
+	fsType = "ext4";
+    };
+
+    # backup drive
     fileSystems."/media/blair/Backup_drive" =
-    { device = "/dev/disk/by-uuid/UUID=a2352a98-821a-4ae4-8a10-a3228d356da5";
+    { 
+	device = "/dev/disk/by-uuid/UUID=e8b1525a-fd36-4590-9c27-efb926306b01";
 	fsType = "ext4";
     };
 
