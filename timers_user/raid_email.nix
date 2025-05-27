076388371a4c012ea70483f2efefb67d2aa9fc2c
blairfix <blairfix@fastmail.com>
2025-05-27 09:29:00 -0600

    raid email

---
+++ timers_user/raid_email.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+{
+
+    # raid email
+    #----------------------------------------
+
+    systemd.timers."raid_email" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-01 04:25:00";
+	    Persistent = "true";
+	    Unit = "raid_email.service";
+	};
+    };
+
+    systemd.services."raid_email" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    WorkingDirectory = "/home/blair/Projects/raid_status/";
+	};
+
+	path = with pkgs; [ 
+	    bash
+	    R
+	];
+	script = ''
+	    bash /home/blair/Projects/raid_status/run.sh
+	    '';
+    };
+}
+
+
